faa341358af4db4d8f5c9e83f303f704e2204d8c
jayanthan-h-21 <jayanthan-h-21@kcs.org.uk>
2024-11-07 21:51:23 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    document.getElementById("latitude").value = latitude.toFixed(6);
+    document.getElementById("longitude").value = longitude.toFixed(6);
+    
+    document.getElementById("verticalLat").value = latitude >= 0 ? "North" : "South";
+    document.getElementById("horizontalLong").value = longitude >= 0 ? "East" : "West";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
